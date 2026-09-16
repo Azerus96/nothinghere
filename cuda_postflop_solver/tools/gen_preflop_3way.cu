8262c4e76f6fc8619dc5c0c6cb2043debebcc5fc
Azerus96 <gencinski1996@gmail.com>
2026-09-16 08:27:40 +0300

    Update gen_preflop_3way.cu

--- cuda_postflop_solver/tools/gen_preflop_3way.cu
+++ cuda_postflop_solver/tools/gen_preflop_3way.cu
@@ -243,7 +243,12 @@ void kernel_3way_mc(
     }
 }
 
-int main(int argc, char** argv) {
+    int main(int argc, char** argv) {
+    if (init_hand_table_on_gpu() != 0) {
+        std::fprintf(stderr, "FATAL: Failed to init GPU hand table\n");
+        return 1;
+    }
+
     bool dry_run = false;
     int samples = 5000;
     std::string out_path = "preflop_3way.bin";
